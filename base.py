4fb6179b42451d5e0c9fd6d139651214ada4fa83
Donatien YETO <donatien.yeto@gmail.com>
2024-05-30 21:58:23 +0200

    feat: add base layers and encoder

---
+++ base.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+
+from multi_head_attention_test import MultiHeadAttention
+
+
+class BaseAttention(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.mha = MultiHeadAttention(**kwargs)
+        self.layer_norm = tf.keras.layers.LayerNormalization()
+        self.add = tf.keras.layers.Add()
+
+
+class CrossAttention(BaseAttention):
+    def call(self, x, values):
+        attn_output = self.mha(
+            q=x,
+            k=values,
+            v=values)
+
+        x = self.add([x, attn_output])
+        x = self.layer_norm(x)
+
+        return x
+
+
+class GlobalSelfAttention(BaseAttention):
+    def call(self, x):
+        attn_output = self.mha(
+            q=x,
+            v=x,
+            k=x)
+        x = self.add([x, attn_output])
+        x = self.layer_norm(x)
+        return x
+
+
+class CausalSelfAttention(BaseAttention):
+    def call(self, x, mask):
+        attn_output = self.mha(
+            q=x,
+            v=x,
+            k=x,
+            mask=mask)
+        x = self.add([x, attn_output])
+        x = self.layer_norm(x)
+        return x
+
+
+class FeedForward(tf.keras.layers.Layer):
+    def __init__(self, d_model, d_ff, dropout_rate=0.1):
+        super().__init__()
+        self.seq = tf.keras.Sequential([
+            tf.keras.layers.Dense(d_ff, activation='relu'),
+            tf.keras.layers.Dense(d_model),
+            tf.keras.layers.Dropout(dropout_rate)
+        ])
+        self.add = tf.keras.layers.Add()
+        self.layer_norm = tf.keras.layers.LayerNormalization()
+
+    def call(self, x):
+        x = self.add([x, self.seq(x)])
+        x = self.layer_norm(x)
+        return x
---
+++ encoder.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+from base import FeedForward, GlobalSelfAttention
+from positional_encoding import PositionalEmbedding
+
+
+class EncoderLayer(tf.keras.layers.Layer):
+    def __init__(self, h, d_k, d_v, d_model, d_ff, dropout_rate=0.1):
+        super().__init__()
+        self.self_attention = GlobalSelfAttention(
+            h=h, d_k=d_k, d_v=d_v, d_model=d_model)
+        self.ffn = FeedForward(d_model, d_ff, dropout_rate)
+
+    def call(self, x):
+        x = self.self_attention(x)
+        x = self.ffn(x)
+        return x
+
+
+class Encoder(tf.keras.layers.Layer):
+    def __init__(self, num_layers, vocab_size, sequence_length, h, d_k, d_v, d_model, d_ff, n=10000, fixed_weights=False, dropout_rate=0.1,):
+        super().__init__()
+
+        self.d_model = d_model
+        self.num_layers = num_layers
+
+        self.pos_embedding = PositionalEmbedding(
+            vocab_size=vocab_size,
+            sequence_length=sequence_length,
+            d_model=d_model,
+            n=n,
+            fixed_weights=fixed_weights
+        )
+
+        self.dropout = tf.keras.layers.Dropout(dropout_rate)
+
+        self.encoder_layers = [
+            EncoderLayer(h, d_k, d_v, d_model, d_ff, dropout_rate)
+            for _ in range(num_layers)
+        ]
+
+    def call(self, x):
+        print(x)
+        x = self.pos_embedding(x)
+
+        x = self.dropout(x)
+
+        for i in range(self.num_layers):
+            x = self.encoder_layers[i](x)
+
+        return x
